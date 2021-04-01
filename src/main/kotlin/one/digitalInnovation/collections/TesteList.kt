932bde91b77ec1c6b43c7cfeb07ac9dbe3393eda
Your Name <you@example.com>
2021-04-01 14:11:32 -0300

    Teste List com kotlin

--- src/main/kotlin/one/digitalInnovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitalInnovation/collections/TesteList.kt
@@ -2,16 +2,29 @@ package one.digitalInnovation.collections
 
 fun main(){
 
-    val joao = Funcionario("Joao" , 1000.00)
-    val maria = Funcionario("Maria" , 1500.00)
+    val joao = Funcionario("Joao" , 1000.00 , "CLT")
+    val maria = Funcionario("Maria" , 10000.00 , "PJ")
+    val pedro = Funcionario("pedro" , 2500.00 , "CLT")
 
-    val funcionarios = listOf(joao,maria)
+    val funcionarios = listOf(joao,maria,pedro)
 
-    funcionarios.forEach { println(it) }
+    funcionarios
+        .forEach { println(it) }
+    println("-------------------------------------")
+
+    funcionarios
+        .sortedBy { it.d  }
+        .forEach { println(it) }
+    println("-------------------------------------")
+    funcionarios
+        .groupBy { it.tipo }
+        .forEach { println(it) }
+
 
 }
 
 data class Funcionario(
    var s: String,
-   var d: Double
+   var d: Double,
+   var tipo:String
 )
