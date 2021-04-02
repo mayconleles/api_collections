3d66de8ab18be55e0058eae93739951999973313
Your Name <you@example.com>
2021-04-01 21:16:07 -0300

    Teste TesteMutavelMap com kotlin

---
+++ src/main/kotlin/one/digitalInnovation/collections/TesteMutavelMap.kt
@@ -0,0 +1,23 @@
+package one.digitalInnovation.collections
+
+fun main (){
+
+    val michael = Employee("michael",5600.00,"CLT")
+    val maria = Employee("maria",4500.00,"CLT")
+    val joao = Employee("joao",2500.00,"pj")
+    val pedro = Employee("pedro",7000.00,"CLT")
+    val matheus = Employee("matheus",1500.00,"pj")
+
+
+    val repository = Repositorio<Employee>()
+
+    repository.create(joao.name,joao)
+    repository.create(michael.name,michael)
+    repository.create(maria.name,maria)
+    repository.create(pedro.name,pedro)
+    repository.create(matheus.name,matheus)
+
+
+    println(repository.findById(joao.name))
+    println(repository.findById(maria.name))
+}
