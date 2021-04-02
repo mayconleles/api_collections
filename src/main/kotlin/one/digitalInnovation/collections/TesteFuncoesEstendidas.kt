9ddc8639b80f07fae08775f4b7c7da376c840269
Your Name <you@example.com>
2021-04-02 11:14:42 -0300

    Teste Funções Estendidas com kotlin

---
+++ src/main/kotlin/one/digitalInnovation/collections/TesteFuncoesEstendidas.kt
@@ -0,0 +1,13 @@
+package one.digitalInnovation.collections
+
+fun main() {
+    val salary = arrayOf(
+        "2000".toBigDecimal(),
+        "1500".toBigDecimal(),
+        "4000".toBigDecimal(),
+    )
+    println("-------------------")
+    println(salary.somatoria())
+    println("-------------------")
+    println(salary.media())
+}
